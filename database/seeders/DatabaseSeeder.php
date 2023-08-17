5080c2cd900ab6bc1b73196c065ec76dce5c4e9a
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-08-17 10:05:16 +0700

    seeder and faker Role Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Company;
+use App\Models\Team;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,13 +17,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
 
-        Company::factory(10)->create();
+        // Company::factory(10)->create();
+        // Team::factory(5)->create();
     }
 }
