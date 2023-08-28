4020c214ddba5ee9caaebd97313c5e623f05e1f0
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-08-28 14:14:02 +0700

    Up Company Controller

--- app/Http/Controllers/API/CompanyController.php
+++ app/Http/Controllers/API/CompanyController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use App\Models\Company;
 use Illuminate\Http\Request;
@@ -11,11 +12,24 @@ class CompanyController extends Controller
    
     public function all(Request $request) {
         $id = $request->input('id');
+        $name = $request->input('name');
         $limit = $request->input('limit', 10);
 
          // powerhuman.com/apy/company?id=1
         if ($id) {
             $company = Company::with(['users'])->find($id);
+
+            if ($company) {
+                return ResponseFormatter::success($company, 'Company found');
+            }
+
+            return ResponseFormatter::error('Company not found', 404);
+        }
+        // powerhuman.com/api/company
+        $companies = Company::with(['users']);
+
+        if ($name) {
+            $companies ->where ('name', 'like', '%' . $name. '%');
         }
     }
 }
