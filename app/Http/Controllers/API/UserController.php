d5f6e3b4442e288a83e7f7c167aef22a1291a108
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-09-03 12:01:27 +0700

    Up User Controller

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -3,11 +3,29 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\TryCatch;
 
 class UserController extends Controller
 {
     public function login(Request $request) {
+        try {
+            // TODO: Validate Request
+            $request->validate([
+                'email' => 'required|email',
+                'password' => 'required'
+            ]);
         
+            // TODO: Find User By Email
+        
+            // TODO: Generate Token
+        
+            // TODO: Return Response
+
+        }catch (Exception $e) {
+
+        }
+
     }
 }
