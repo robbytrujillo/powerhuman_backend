95266812409378b00c63965a2f7869f44bbb5c9e
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-09-06 11:54:14 +0700

    Up User Controller

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\API;
 
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use PhpParser\Node\Stmt\TryCatch;
 
 class UserController extends Controller
@@ -24,12 +26,25 @@ class UserController extends Controller
             if (!Auth::attempt($credentials)) {
                 return ResponseFormatter::error('Unauthorized', 401);
             }
+
+            $user = User::where('email', $request->email)->first();
+            if (!Hash::check($request->password, $user->password)) {
+                throw new Exception('Invalid password');
+            }
+
             // TODO: Generate Token
+            $tokenResult = $user->createToen('authToken')->plainTextToken;
+            
         
             // TODO: Return Response
-
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ], 'Login success');
+            
         }catch (Exception $e) {
-
+            return ResponseFormatter::error('Authentication Failed');
         }
 
     }
