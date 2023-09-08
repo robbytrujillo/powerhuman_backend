c7c18084957a98373640ce5d5408e24b6da9dbd0
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-09-08 13:24:21 +0700

    update routes | login

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\CompanyController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/company', [CompanyController::class, 'all']);
+
+// login (mengirim sesuatu dan mendapatkan sesuatu)
+Route::post('login',[UserController::class, 'login']);
